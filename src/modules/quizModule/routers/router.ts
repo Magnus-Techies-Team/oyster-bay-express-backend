6bd7bfbac21faffa9a89d6c3bddae076fbbe337d
Shiron <shiron.kaz@gmail.com>
2022-06-21 22:11:49 +0300

    Remove: redundant code | Update: according to the current architecture

--- src/modules/quizModule/routers/router.ts
+++ src/modules/quizModule/routers/router.ts
@@ -2,8 +2,6 @@ import { verifyJWTHook } from "../../utils/verifyJWThook";
 import { RegisterOptions, RouteHandlerMethod } from "fastify";
 import { RouteOptions } from "@fastify/websocket";
 import * as controller from "./controller";
-import { joinLobby } from "../../lobbyModule/routers/lobbyController";
-import { WebsocketHandler } from "@fastify/websocket";
 
 export const opts: RegisterOptions = {
   prefix: "/quiz",
@@ -15,24 +13,5 @@ export const routes: RouteOptions[] = [
     url: "/createQuiz",
     handler: <RouteHandlerMethod>controller.createQuiz,
     preValidation: verifyJWTHook,
-    wsHandler: <WebsocketHandler>joinLobby,
-  },
-  {
-    method: "POST",
-    url: "/createLobby",
-    handler: <RouteHandlerMethod>controller.createLobby,
-  },
-  {
-    method: "GET",
-    url: "/joinLobby",
-    handler: () => {
-      console.log("");
-    },
-    wsHandler: <WebsocketHandler>joinLobby,
-  },
-  {
-    method: "POST",
-    url: "/disconnectLobby",
-    handler: <RouteHandlerMethod>controller.disconnectLobby,
   },
 ];
--- src/modules/usersModule/utils/UserManager.ts
+++ src/modules/usersModule/utils/UserManager.ts
@@ -1,36 +1,35 @@
-import { ServiceClass } from "../../../utils/serviceClass";
+import ServiceClass from "../../../utils/serviceClass";
 
 class UserManager {
   public async createUser(userData: {
-    login: string,
-    password: string,
-    email: string
+    login: string;
+    password: string;
+    email: string;
   }) {
-    const emailMatcher = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+    const emailMatcher =
+      /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
     if (emailMatcher.exec(userData.email)) {
-      const userCreated = await new ServiceClass().createRecord({
+      const userCreated = await ServiceClass.createRecord({
         tableName: "users",
-        columnObject: userData
+        columnObject: userData,
       });
       return userCreated.rows[0];
     }
-    return { error: `Invalid email pattern`};
+    return { error: `Invalid email pattern` };
   }
 
-  public async login(userData: {
-    login: string, password: string
-  }) {
-    const user = await new ServiceClass().getRecord({
+  public async login(userData: { login: string; password: string }) {
+    const user = await ServiceClass.getRecord({
       tableName: "users",
       searchBy: "login",
-      value: userData.login
+      value: userData.login,
     });
     if (user.error) {
       return { error: user.error };
     } else if (userData.password !== user.rows[0].password) {
-      return { error: `Invalid password`};
+      return { error: `Invalid password` };
     } else {
-      const data = { ...user.rows[0]};
+      const data = { ...user.rows[0] };
       delete data.password;
       return data;
     }
