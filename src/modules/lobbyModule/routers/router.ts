3a031c6f2d9270e6cdf8e9aaa5c22e4a54c5aaa7
Shiron <shiron.kaz@gmail.com>
2022-08-24 21:10:08 +0300

    Remove: redundant code

--- src/modules/lobbyModule/routers/router.ts
+++ src/modules/lobbyModule/routers/router.ts
@@ -1,6 +1,5 @@
-import { RegisterOptions, RouteHandlerMethod } from "fastify";
+import { RegisterOptions } from "fastify";
 import { RouteOptions } from "@fastify/websocket";
-import * as controller from "./controller";
 import * as socketController from "./lobbyController";
 import { WebsocketHandler } from "@fastify/websocket";
 
@@ -16,23 +15,5 @@ export const routes: RouteOptions[] = [
       console.log("Lobby created");
     },
     wsHandler: <WebsocketHandler>socketController.setConnection,
-  },
-  {
-    method: "GET",
-    url: "/joinLobby",
-    handler: (): void => {
-      console.log("Client joined");
-    },
-    wsHandler: <WebsocketHandler>socketController.joinLobby,
-  },
-  {
-    method: "POST",
-    url: "/disconnectLobby",
-    handler: <RouteHandlerMethod>controller.disconnectLobby,
-  },
-  {
-    method: "POST",
-    url: "/startLobby",
-    handler: controller.startLobby,
   },
 ];
